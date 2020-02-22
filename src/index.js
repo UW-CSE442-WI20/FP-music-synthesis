2d972796a506b1fd145bd3e8f5a146460f7087ab
Jialiang Liu <neicebanben@gmail.com>
2020-02-21 16:17:21 -0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+
+
+// You can require libraries
+const d3 = require('d3')
+
+// You can include local JS files:
+const MyClass = require('./my-class');
+const myClassInstance = new MyClass();
+myClassInstance.sayHi();
+
+
+// You can load JSON files directly via require.
+// Note this does not add a network request, it adds
+// the data directly to your JavaScript bundle.
+const exampleData = require('./example-data.json');
+
+
+// Anything you put in the static folder will be available
+// over the network, e.g.
+d3.csv('carbon-emissions.csv')
+  .then((data) => {
+    console.log('Dynamically loaded CSV data', data);
+  })
+
---
+++ src/my-class.js
@@ -0,0 +1,16 @@
+
+
+// You can separate your code out into modules to
+// keep code clean.
+
+class MyClass {
+  constructor() {
+
+  }
+
+  sayHi() {
+    console.log('[MyClass]', 'Hello World.');
+  }
+}
+
+module.exports = MyClass;
