51db015577edb72cd9e786e548b9ee612f6ad608
Jaclyn Brockschmidt <jcbrockschmidt@gmail.com>
2020-03-02 02:34:45 -0800

    Add function for getting envelope curve data

--- src/index.js
+++ src/index.js
@@ -47,6 +47,56 @@ function openPage(pageName) {
     document.getElementById(pageName).style.display = "block";
 }
 
+
+async function getEnvelopeCurve(t) {
+  const n = t.attack + t.decay + t.release;
+  const params = t.get();
+
+  // Simulate envelope to get curve data
+  const points = await Tone.Offline(() => {
+    const dummy = new Tone.Envelope;
+    dummy.set(params);
+    dummy.attack *= 1, dummy.decay *= 1, dummy.release *= 1;
+    dummy.toMaster();
+    dummy.triggerAttack(.001);
+    dummy.triggerRelease(1 * (dummy.attack + dummy.decay + .01) + .001);
+  }, 1 * (n + .01) + .002);
+
+  return points.toArray(0);
+}
+
+function initEnvelope() {
+  var envl = new Tone.Envelope().toMaster();
+
+  // set the dimensions and margins of the graph
+  var margin = {top: 10, right: 30, bottom: 30, left: 60},
+      width = 460 - margin.left - margin.right,
+      height = 400 - margin.top - margin.bottom;
+
+  // append the svg object to the body of the page
+  var svg = d3.select("#envelope-viz")
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+  // TODO: x limit should be updated dynamically
+  var length = envl.attack + envl.decay + envl.release;
+  var xAxis = d3.scaleLinear().range([0, length]);
+  var yAxis = d3.scaleLinear().range([0, 1]);
+  line = d3.line()
+    .x(d => xScale(d.time))
+    .y(d => yScale(d.value))
+    .curve(d3.curveLinear)
+
+  getEnvelopeCurve(envl).then(function(d) {
+    // TODO: Push these points to a plot
+    console.log(points);
+  });
+}
+
 $(document).ready(function() {
   document.getElementById("defaultOpen").click();
 
@@ -65,4 +115,7 @@ $(document).ready(function() {
       }
     });
   }
+
+  // Envelope
+  initEnvelope();
 });
