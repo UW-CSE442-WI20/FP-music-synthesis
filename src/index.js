58c29b53aa5c4626705fae1ee06a6cd6acc3fad4
Jaclyn Brockschmidt <jcbrockschmidt@gmail.com>
2020-03-02 20:55:52 -0800

    Remove example code

--- src/index.js
+++ src/index.js
@@ -1,28 +1,6 @@
-// GIVEN STUFF IN TEMPLATE
-// // You can require libraries
 import * as Tone from "tone";
 const d3 = require('d3')
 import $ from 'jquery'
-
-//
-// // You can include local JS files:
-// const MyClass = require('./my-class');
-// const myClassInstance = new MyClass();
-// myClassInstance.sayHi();
-//
-//
-// // You can load JSON files directly via require.
-// // Note this does not add a network request, it adds
-// // the data directly to your JavaScript bundle.
-// const exampleData = require('./example-data.json');
-//
-//
-// // Anything you put in the static folder will be available
-// // over the network, e.g.
-// d3.csv('carbon-emissions.csv')
-//   .then((data) => {
-//     console.log('Dynamically loaded CSV data', data);
-//   })
 
 // Sidebar
 var mini = true;
