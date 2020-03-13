d6994dceb552e632b942992fa4e7f4c729ec1da5
Jaclyn Brockschmidt <jcbrockschmidt@gmail.com>
2020-03-13 00:41:22 -0700

    Update published site

--- docs/src.4b4dc70c.js
+++ docs/src.4b4dc70c.js
@@ -29534,6 +29534,17 @@ var synth = require("./synth.js");
 document.addEventListener("DOMContentLoaded", function () {
   var sliderWidth = 300;
   var sliderHeight = 75;
+  var strokeWidth = 2;
+  var envl = new Tone.Envelope();
+  var margin = {
+    top: 10,
+    right: 30,
+    bottom: 30,
+    left: 60
+  };
+  var width = 460 - margin.left - margin.right;
+  var height = 150 - margin.top - margin.bottom;
+  var svg = d3.select("#envelope-viz").append("svg").attr("width", width + margin.left + margin.right).attr("height", height + margin.top + margin.bottom).append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
   function getEnvelopeCurve(envl, callback) {
     var n = envl.attack + envl.decay + envl.release;
@@ -29552,7 +29563,16 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-  function initEnvelopeCurve(envl) {
+  function updateDivideLine() {
+    var beforeRelease = envl.attack + envl.decay;
+    var fullTime = beforeRelease + envl.release;
+    var divideX = width * beforeRelease / fullTime;
+    svg.select("line").attr("x1", divideX).attr("y1", 0).attr("x2", divideX).attr("y2", height);
+    svg.select("text").attr("x", divideX + 5);
+  }
+
+  function initEnvelopeCurve() {
+    synth.setEnvelope(envl);
     getEnvelopeCurve(envl, function (points) {
       var x = d3.scaleLinear().domain([0, points.length]).range([0, width]);
       var y = d3.scaleLinear().domain([1, 0]).range([0, height]);
@@ -29560,12 +29580,17 @@ document.addEventListener("DOMContentLoaded", function () {
         return x(i);
       }).y(function (d) {
         return y(d);
-      });
-      svg.append("path").attr("fill", "none").attr("stroke", "black").attr("stroke-width", 1.5).attr("d", line(points));
+      }); // Draw envelope plot
+
+      svg.append("path").attr("fill", "none").attr("stroke", "black").attr("stroke-width", strokeWidth).attr("d", line(points)); // Draw divide line between hold and release
+
+      svg.append("line").style("stroke", "#000").attr("stroke-width", strokeWidth).attr("opacity", 0.2).style("stroke-dasharray", "3, 3");
+      svg.append("text").attr("class", "x label").attr("text-anchor", "start").attr("y", 0).text(String.fromCharCode(0x2190) + " before release");
+      updateDivideLine();
     });
   }
 
-  function updateEnvelopeCurve(envl) {
+  function updateEnvelopeCurve() {
     synth.setEnvelope(envl);
     getEnvelopeCurve(envl, function (points) {
       var x = d3.scaleLinear().domain([0, points.length]).range([0, width]);
@@ -29576,38 +29601,29 @@ document.addEventListener("DOMContentLoaded", function () {
         return y(d);
       });
       svg.selectAll("path").attr("d", line(points));
+      updateDivideLine();
     });
-  }
+  } // Sliders
 
-  var envl = new Tone.Envelope();
-  var margin = {
-    top: 10,
-    right: 30,
-    bottom: 30,
-    left: 60
-  };
-  var width = 460 - margin.left - margin.right;
-  var height = 150 - margin.top - margin.bottom;
-  var svg = d3.select("#envelope-viz").append("svg").attr("width", width + margin.left + margin.right).attr("height", height + margin.top + margin.bottom).append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")"); // Sliders
 
   var attackSlider = (0, _d3SimpleSlider.sliderHorizontal)().min(0.01).max(2).width(sliderWidth).displayValue(true).tickFormat(d3.format('.2')).default(envl.attack).on('onchange', function (val) {
     envl.attack = val;
-    updateEnvelopeCurve(envl);
+    updateEnvelopeCurve();
   });
   d3.select('#envelope-attack-slider').append('svg').attr('width', sliderWidth + 50).attr('height', sliderHeight).append('g').attr('transform', 'translate(30,30)').call(attackSlider);
   var decaySlider = (0, _d3SimpleSlider.sliderHorizontal)().min(0.01).max(2).width(sliderWidth).displayValue(true).tickFormat(d3.format('.2')).default(envl.decay).on('onchange', function (val) {
     envl.decay = val;
-    updateEnvelopeCurve(envl);
+    updateEnvelopeCurve();
   });
   d3.select('#envelope-decay-slider').append('svg').attr('width', sliderWidth + 50).attr('height', sliderHeight).append('g').attr('transform', 'translate(30,30)').call(decaySlider);
-  var sustainSlider = (0, _d3SimpleSlider.sliderHorizontal)().min(0.01).max(1).width(sliderWidth).displayValue(true).tickFormat(d3.format('.2')).default(envl.sustain).on('onchange', function (val) {
+  var sustainSlider = (0, _d3SimpleSlider.sliderHorizontal)().min(0).max(1).width(sliderWidth).displayValue(true).tickFormat(d3.format('.2')).default(envl.sustain).on('onchange', function (val) {
     envl.sustain = val;
-    updateEnvelopeCurve(envl);
+    updateEnvelopeCurve();
   });
   d3.select('#envelope-sustain-slider').append('svg').attr('width', sliderWidth + 50).attr('height', sliderHeight).append('g').attr('transform', 'translate(30,30)').call(sustainSlider);
   var releaseSlider = (0, _d3SimpleSlider.sliderHorizontal)().min(0.01).max(4).width(sliderWidth).displayValue(true).tickFormat(d3.format('.2')).default(envl.release).on('onchange', function (val) {
     envl.release = val;
-    updateEnvelopeCurve(envl);
+    updateEnvelopeCurve();
   });
   d3.select('#envelope-release-slider').append('svg').attr('width', sliderWidth + 50).attr('height', sliderHeight).append('g').attr('transform', 'translate(30,30)').call(releaseSlider); // Curve type selection
 
@@ -29627,7 +29643,7 @@ document.addEventListener("DOMContentLoaded", function () {
     var select = document.getElementById(id);
     select.addEventListener('change', function () {
       envl[attr] = select.value;
-      updateEnvelopeCurve(envl);
+      updateEnvelopeCurve();
     });
   }
 
@@ -29637,7 +29653,7 @@ document.addEventListener("DOMContentLoaded", function () {
   populateSelect('envelope-attack-select', attackCurveTypes, 'attackCurve');
   populateSelect('envelope-decay-select', decayCurveTypes, 'decayCurve');
   populateSelect('envelope-release-select', releaseCurveTypes, 'releaseCurve');
-  initEnvelopeCurve(envl);
+  initEnvelopeCurve();
 });
 },{"tone":"DNic","d3":"UzF0","d3-simple-slider":"Sf04","./synth.js":"EAfG"}],"ojOq":[function(require,module,exports) {
 var keyboard = require("./keyboard.js");
@@ -29978,4 +29994,4 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 },{"d3":"UzF0","./keyboard.js":"VgNd","./envelope.js":"AP8s","./midi.js":"ojOq","./soundwave.js":"ZngV","./pitch.js":"sGps"}]},{},["Focm"], null)
-//# sourceMappingURL=https://uw-cse442-wi20.github.io/FP-music-synthesis/src.35a5ac9f.js.map
+//# sourceMappingURL=https://uw-cse442-wi20.github.io/FP-music-synthesis/src.4b4dc70c.js.map
